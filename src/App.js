e1bddb4d7063f6f851c28b8c83bfd92d0843e179
quangtv13 <quangtv13@fsoft.com.vn>
2019-07-23 23:52:50 +0700

    017

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TaskForm from './components/TaskForm';
+import Control from './components/Control';
+import TaskList from './components/TaskList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="container">
+			<div className="text-center">
+				<h1>Quản Lý Công Việc</h1>
+				<hr />
+			</div>
+			<div className="row">
+				<div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+					{/* Form */}
+					<TaskForm />
+				</div>
+				<div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+					<button type="button" className="btn btn-primary">
+						<span className="fa fa-plus mr-5" />Thêm Công Việc
+		  </button>
+					<Control />
+					<TaskList />
+				</div>
+			</div>
+		</div>
+
+	);
 }
 
 export default App;
