53ebad8cce08d646d9c08efc7537590318ebb837
quangtv13 <quangtv13@fsoft.com.vn>
2019-07-25 22:34:56 +0700

    Updates

---
+++ precache-manifest.649178a0123f6e479f9d3fd1d35ed264.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df3b66745c2bfe14978726062c5014a6",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "d874a7833ec2288e054a",
+    "url": "/todolist/static/css/main.684ae7af.chunk.css"
+  },
+  {
+    "revision": "774d5e3722e17f42d013",
+    "url": "/todolist/static/js/2.cba05053.chunk.js"
+  },
+  {
+    "revision": "d874a7833ec2288e054a",
+    "url": "/todolist/static/js/main.94e2db85.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
